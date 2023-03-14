f74174f8c1a81519ac0a3fb448e0a93a623a6fa7
ritul bhatnagar <ritulbhatnagar375@gamil.com>
2023-03-14 20:11:30 +0530

    some

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://spontaneous-tanuki-a34d0e.netlify.app/"],
+    origin: ["http://localhost:3000", "https://inventoryfrontend.onrender.com/"],
     credentials: true,
   })
 );
